fe5099ef2eddcfc77629b1b43afa52a0d29fb3ce
Cypek-exe <105518009+cypek-exe@users.noreply.github.com>
2024-01-07 17:54:08 +0100

    File is more readable using better variable names and better SQL query notation

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once 'conn-data.php';
 require_once 'header.php';
 require_once 'content.php';
 require_once 'exercises.php';
 require_once 'footer.php';
+
+$cd = require_once 'conn-data.php';
 
 $conn = new mysqli(
   $cd['host'],
@@ -20,45 +21,115 @@ if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO']) {
 
   $subject = $path_array[0];
 
-  $query1 = "SELECT id, alias, name FROM subjects WHERE alias = '$subject'";
-  $result1 = $conn -> query($query1);
+  $subject_query = <<< EOD
+    SELECT ID, Alias, Name 
+    FROM Subjects 
+    WHERE Alias = '$subject';
+    EOD;
 
-  if ($result1 -> num_rows) {
-    $row1 = $result1 -> fetch_assoc();
+  $subject_result = $conn -> query($subject_query);
+
+  if ($subject_row = $subject_result -> fetch_assoc()) {
+
+    $subject_id    = $subject_row['ID'];
+    $subject_alias = $subject_row['Alias'];
+    $subject_name  = $subject_row['Name'];
 
     if (isset($path_array[1])) {
       $exercise = $path_array[1];
-      $query2 = "SELECT alias, title, subtitle, custom_button_set FROM exercises WHERE alias = '$exercise'";
-      $result2 = $conn -> query($query2);
 
-      if ($row2 = $result2 -> fetch_assoc()) {
+      $exercise_query = <<< EOD
+        SELECT ID, Alias, Title, Subtitle, Type
+        FROM Exercises
+        WHERE Subject_ID = $subject_id AND Alias = '$exercise';
+        EOD;
 
-        get_header('ZS10 Ćwiczenia - '.$row2['title']);
+      $exercise_result = $conn -> query($exercise_query);
 
-        print_translation(
-          $row1['name'],
-          $row2['title'],
-          $row2['subtitle'],
-          $row2['custom_button_set']
-        );
+      if ($exercise_row = $exercise_result -> fetch_assoc()) {
+        
+        $exercise_id       = $exercise_row['ID'];
+        $exercise_title    = $exercise_row['Title'];
+        $exercise_subtitle = $exercise_row['Subtitle'];
+        $exercise_type     = $exercise_row['Type'];
+
+        get_header("ZS10 Ćwiczenia - $exercise_title");
+
+        switch ($exercise_type) {
+          case 0:
+            $translation_exercise_query = <<< EOD
+              SELECT Custom_Button_Set
+              FROM Translations_Exercises
+              WHERE Exercise_ID = $exercise_id;
+              EOD;
+
+            $translation_exercise_result = $conn -> query($translation_exercise_query);
+
+            if ($translation_exercise_row = $translation_exercise_result -> fetch_assoc())
+              print_translation(
+                $subject_name,
+                $exercise_title,
+                $exercise_subtitle,
+                $translation_exercise_row['Custom_Button_Set']
+              );
+            break;
+
+          case 1:
+            $map_exercise_query = <<< EOD
+              SELECT
+                IoM.Directory AS Map_Image_Dir,
+                ME.Map_HTML_Code
+              FROM Maps_Exercises      ME
+              LEFT JOIN Images_of_Maps IoM
+                ON ME.Map_Image_ID = IoM.ID
+              WHERE ME.Exercise_ID = $exercise_id;
+              EOD;
+
+            $map_exercise_result = $conn -> query($map_exercise_query);
+
+            if ($map_exercise_row = $map_exercise_result -> fetch_assoc())
+              print_map(
+                $subject_name,
+                $exercise_title,
+                $exercise_subtitle,
+                $map_exercise_row['Map_Image_Dir'],
+                $map_exercise_row['Map_HTML_Code']
+              );
+            break;
+
+          default:
+            echo 'Nieznane ćwiczenie';
+            break;
+        }
+
       } else {
-        header('Location: /'.$row1['alias']);
+        header("Location: /$subject_alias");
       }
 
     } else { 
-      $query2 = "SELECT alias, title, subtitle, description FROM exercises WHERE subject_id = {$row1['id']}";
-      $result2 = $conn -> query($query2);
+      $exercises_query = <<< EOD
+        SELECT Alias, Title, Subtitle, Description 
+        FROM Exercises
+        WHERE Subject_ID = $subject_id;
+        EOD;
 
-      get_header('ZS10 Ćwiczenia - '.$row1['name']);
+      $exercises_result = $conn -> query($exercises_query);
+
+      get_header("ZS10 Ćwiczenia - $subject_name");
 
       echo '<section class="cards" id="exercises">'."\n";
 
-      while ($row2 = $result2 -> fetch_assoc()) {
+      while ($exercises_row = $exercises_result -> fetch_assoc()) {
+        $exercises_alias       = $exercises_row['Alias'];
+        $exercises_title       = $exercises_row['Title'];
+        $exercises_subtitle    = $exercises_row['Subtitle'];
+        $exercises_description = $exercises_row['Description'];
+
         print_exercise(
-          '/'.$row1['alias'].'/'.$row2['alias'],
-          $row2['title'],
-          $row2['subtitle'],
-          $row2['description']
+          "/$subject_alias/$exercises_alias",
+          $exercises_title,
+          $exercises_subtitle,
+          $exercises_description
         );
       }
 
@@ -69,19 +140,28 @@ if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO']) {
   }
 
 } else {
-  $query = 'SELECT alias, name, description, image FROM subjects';
-  $result = $conn -> query($query);
+  $subjects_query = <<< EOD
+    SELECT Alias, Name, Description, Image 
+    FROM Subjects;
+    EOD;
+
+  $subjects_result = $conn -> query($subjects_query);
   
-  get_header("ZS10 Ćwiczenia");
+  get_header('ZS10 Ćwiczenia');
 
   echo '<section class="cards">'."\n";
 
-  while ($row = $result -> fetch_assoc()) {
+  while ($subjects_row = $subjects_result -> fetch_assoc()) {
+    $subjects_alias       = $subjects_row['Alias'];
+    $subjects_name        = $subjects_row['Name'];
+    $subjects_description = $subjects_row['Description'];
+    $subjects_image       = $subjects_row['Image'];
+
     print_subject(
-      '/'.$row['alias'],
-      $row['image'],
-      $row['name'],
-      $row['description']
+      "/$subjects_alias",
+      $subjects_image,
+      $subjects_name,
+      $subjects_description
     );
   }
 
